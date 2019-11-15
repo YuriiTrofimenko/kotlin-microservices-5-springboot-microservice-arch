6706e068431ba5b95b54048a9318198ff7260a32
YuriiTrofimenko <tyaa@ukr.net>
2019-11-15 11:10:51 +0200

    eureka-discovery-server

--- eureka-discovery-server/src/main/kotlin/org/tyaa/eurekadiscoveryserver/EurekaDiscoveryServerApplication.kt
+++ eureka-discovery-server/src/main/kotlin/org/tyaa/eurekadiscoveryserver/EurekaDiscoveryServerApplication.kt
@@ -2,10 +2,8 @@ package org.tyaa.eurekadiscoveryserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
-@EnableEurekaServer
 class EurekaDiscoveryServerApplication
 
 fun main(args: Array<String>) {
